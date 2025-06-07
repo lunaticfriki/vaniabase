09ee867591c5a86f7b13d94322d76f7f4bd47924
Oscar Ventura <ovenor11@proton.me>
2025-06-07 11:30:28 +0200

    fonts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,11 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'vaniabase';
+  year = new Date().getFullYear();
+  nickname = 'lunaticfriki';
+  githubUrl = 'http://www.github.com/lunaticfriki';
 }
