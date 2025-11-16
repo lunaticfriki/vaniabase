65b890d104d96afcad11b37c5139bd508f0b641a
Oscar Ventura <lunaticfriki@gmail.com>
2025-11-16 17:41:10 +0000

    feat: home link

--- src/presentation/components/header.component.tsx
+++ src/presentation/components/header.component.tsx
@@ -1,10 +1,16 @@
+import { Link } from 'react-router-dom';
 import { MenuComponent } from './menu.component';
 
 export const HeaderComponent = () => {
   return (
     <header className="flex items-center justify-between p-4 text-pink-500">
       <div className="flex-1"></div>
-      <h1 className="text-4xl flex-1 text-center">VANIABASE</h1>
+      <Link
+        to="/"
+        className="flex-1 text-center hover:opacity-80 transition-opacity"
+      >
+        <h1 className="text-4xl">VANIABASE</h1>
+      </Link>
       <div className="flex-1 flex justify-end">
         <MenuComponent />
       </div>
