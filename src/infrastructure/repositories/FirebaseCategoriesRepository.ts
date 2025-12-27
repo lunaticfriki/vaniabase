55d860b664b10f046d588fae0000e3e022bb6b20
lunaticfriki <ovenor11@proton.me>
2025-12-27 23:53:25 +0100

    firebase

--- src/infrastructure/repositories/FirebaseCategoriesRepository.ts
+++ src/infrastructure/repositories/FirebaseCategoriesRepository.ts
@@ -1,21 +1,46 @@
-import type { Category } from "../../domain/model/entities/Category";
-import type { Id } from "../../domain/model/value-objects/Id";
-import type { CategoriesRepository } from "../../domain/repositories/CategoriesRepository";
+import { Category } from "../../domain/model/entities/Category";
+import { Id } from "../../domain/model/value-objects/Id";
+import { Title } from "../../domain/model/value-objects/StringValues";
+import { CategoriesRepository } from "../../domain/repositories/CategoriesRepository";
 import { injectable } from "inversify";
+import { db } from "../firebase/firebaseConfig";
+import { collection, doc, getDoc, getDocs, setDoc } from "firebase/firestore";
 
 @injectable()
 export class FirebaseCategoriesRepository implements CategoriesRepository {
-    constructor() {
-        console.log("FirebaseCategoriesRepository initialized");
-    }
+    private readonly collectionName = "categories";
+
+    constructor() {}
 
     async save(category: Category): Promise<void> {
-        throw new Error("Method not implemented.");
+        const categoryRef = doc(db, this.collectionName, category.id.value);
+        await setDoc(categoryRef, {
+            name: category.name.value
+        });
     }
+
     async findAll(): Promise<Category[]> {
-        throw new Error("Method not implemented.");
+        const querySnapshot = await getDocs(collection(db, this.collectionName));
+        return querySnapshot.docs.map(doc => {
+            const data = doc.data();
+            return Category.create(
+                Id.create(doc.id),
+                Title.create(data.name)
+            );
+        });
     }
+
     async findById(id: Id): Promise<Category | undefined> {
-        throw new Error("Method not implemented.");
+        const docRef = doc(db, this.collectionName, id.value);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            const data = docSnap.data();
+            return Category.create(
+                Id.create(docSnap.id),
+                Title.create(data.name)
+            );
+        }
+        return undefined;
     }
 }
